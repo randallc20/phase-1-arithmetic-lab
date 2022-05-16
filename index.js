3665a2c0e7ec12cac4352b175d9e65cf26fd2554
chrisrandall <chris.f.randall@gmail.com>
2022-05-16 13:43:21 -0600

    commit tree 66e5e92da6c73c40094b84c06fa418ebf3ee007a

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 104;
+let num4 = 25;
+let mod = num3 % num4;
+
+let numberSet = [1,2,3,4,5,20];
+let max = Math.max(numberSet);
+//let max = Math.max(1,2,3,4,5,20);
